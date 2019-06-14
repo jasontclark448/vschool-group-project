750a9d651a16ea0987467b3d0441eaecee637d1e
Samuel Santibout <santibout@yahoo.com>
2019-06-14 16:31:24 -0600

    made video user interaction schema

--- Models/Movie.js
+++ Models/Movie.js
@@ -2,17 +2,21 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const videoSchema = new Schema({
-    title: {
+    author: {
         type: String,
-        required: true
-    },
-    year: {
-        type: Number
+        required: String
     },
     vote: {
         type: Number,
         default: 0
-    }
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    thumbnail: String,
+    uploadDate: String,
 })
 
 module.exports = mongoose.model("Movie", videoSchema)

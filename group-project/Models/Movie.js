61ee6208f2c00851929e09ce871915ff75116e4f
Jason Clark <mailjasontclark@gmail.com>
2019-06-18 10:04:41 -0600

    idk

---
+++ group-project/Models/Movie.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const videoSchema = new Schema({
+    author: {
+        type: String,
+        required: String
+    },
+    vote: {
+        type: Number,
+        default: 0
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    thumbnail: String,
+    uploadDate: String,
+})
+
+module.exports = mongoose.model("Movie", videoSchema)

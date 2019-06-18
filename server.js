e3853ae2d963dff60c1afa383153e8a98defc659
Samuel Santibout <santibout@yahoo.com>
2019-06-18 17:09:27 -0600

    changed git ignore

--- server.js
+++ server.js
@@ -66,33 +66,33 @@ app.use(express.json())
 
 // app.use('/videos', require('./routes/VideoRoutes.js')) 
 
-app.post('/videos', function (req, res) {
-    const newMovie = new Movie(req.body)
-    newMovie.save((err, savedMovie) => {
-        console.log('here i go');
-        if (err) {
-            next(err);
-        } else {
-            ins = fs.createReadStream(files.file.path);
-            console.log('insssssssssssss' + ins);
-            ous = fs.createWriteStream(__dirname + '/static/uploads/videos/' + files.file.filename);
-            // util.pump(ins, ous, function (err) {
-            //     if (err) {
-            //         next(err);
-            //     } else {
-            //         RegProvider.save({
-            //             file: req.param(files.file.filename),
-            //             filename: req.param('filename')
-            //         }, function (error, docs) {
-            //             res.redirect('/videos');
-            //         });
-            //     }
-            // });
-            //console.log('\nUploaded %s to %s', files.file.filename, files.file.path);
-            //res.send('Uploaded ' + files.file.filename + ' to ' + files.file.path);
-        }
-    });
-});
+// app.post('/videos', function (req, res) {
+//     const newMovie = new Movie(req.body)
+//     newMovie.save((err, savedMovie) => {
+//         console.log('here i go');
+//         if (err) {
+//             next(err);
+//         } else {
+//             ins = fs.createReadStream(files.file.path);
+//             console.log('insssssssssssss' + ins);
+//             ous = fs.createWriteStream(__dirname + '/static/uploads/videos/' + files.file.filename);
+//             // util.pump(ins, ous, function (err) {
+//             //     if (err) {
+//             //         next(err);
+//             //     } else {
+//             //         RegProvider.save({
+//             //             file: req.param(files.file.filename),
+//             //             filename: req.param('filename')
+//             //         }, function (error, docs) {
+//             //             res.redirect('/videos');
+//             //         });
+//             //     }
+//             // });
+//             //console.log('\nUploaded %s to %s', files.file.filename, files.file.path);
+//             //res.send('Uploaded ' + files.file.filename + ' to ' + files.file.path);
+//         }
+//     });
+// });
 
 app.get('/', (req, res) => {
     res.render('index')
